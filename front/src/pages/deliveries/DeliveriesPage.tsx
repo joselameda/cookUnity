394747fbfa7a85f9b51ee81bd1a3eb11d7ffb723
Jose Gonzalez <josegonzlameda@gmail.com>
2024-04-30 17:24:09 -0300

    first commit

---
+++ front/src/pages/deliveries/DeliveriesPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Header } from './Header/Header.tsx';
+import { Main } from './Main/Main.tsx';
+
+export const DeliveriesPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
